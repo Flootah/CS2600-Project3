5718ac62b37a81c79b00a871d0485fabdbf53647
flootah <ed.saenz@protonmail.ch>
2021-10-05 18:00:37 -0700

    chainging to more skeletony skeleton code
    as given by professor, and not the linked project website.

--- address_book_menu.c
+++ address_book_menu.c
@@ -107,6 +107,7 @@ Status menu(AddressBook *address_book)
 		{
 			case e_add_contact:
 				/* Add your implementation to call add_contacts function here */
+				search_contact(address_book);
 				break;
 			case e_search_contact:
 				search_contact(address_book);
